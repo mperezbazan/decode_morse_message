5493e1476d20a8637343acbd317b931e440e2710
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-01-26 01:21:50 +0400

    ADD decode word and message methods

--- decode_message.rb
+++ decode_message.rb
@@ -32,3 +32,17 @@ def decode_char(char)
     return key.upcase if code == char
   end
 end
+
+def decode_word(word)
+  decoded_word = ''
+  characters = word.split
+  characters.each { |c| decoded_word = "#{decoded_word}#{decode_char(c)}" }
+  decoded_word
+end
+
+def decode(message)
+  decoded_message = ''
+  words = message.split('   ')
+  words.each { |w| decoded_message = "#{decoded_message} #{decode_word(w)}" }
+  decoded_message.strip
+end
