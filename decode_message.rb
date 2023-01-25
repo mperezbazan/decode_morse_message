9271256fdb59e2cc1e76eb3bc2ea1b277566d71c
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-01-25 18:16:41 -0300

    Add decode char function

---
+++ decode_message.rb
@@ -0,0 +1,34 @@
+MORSE_CODE = {
+  a: '.-',
+  b: '-...',
+  c: '_.-.',
+  d: '-..',
+  e: '.',
+  f: '..-.',
+  g: '--.',
+  h: '....',
+  i: '..',
+  j: '.---',
+  k: '-.-',
+  l: '.-..',
+  m: '--',
+  n: '-.',
+  o: '---',
+  p: '.--.',
+  q: '--.-',
+  r: '.-.',
+  s: '...',
+  t: '-',
+  u: '..-',
+  v: '...-',
+  w: '.--',
+  x: '-..-',
+  y: '-.--',
+  z: '--..'
+}.freeze
+
+def decode_char(char)
+  MORSE_CODE.each do |key, code|
+    return key.upcase if code == char
+  end
+end
